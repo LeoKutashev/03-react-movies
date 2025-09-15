e7ee5acac0e66fe78da4bdcd59a7106f93bdff0e
Леонiд Куташев <leo.kutashev@gmail.com>
2025-09-15 20:48:21 +0200

    Fix

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,24 +1,26 @@
 import axios from "axios";
 import type { Movie } from "../types/movie";
 
-axios.defaults.baseURL = "https://api.themoviedb.org/";
+interface MovieHttpResponse {
+  results: Movie[];
+}
 
-const TOKEN: string =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZTFmNjllZTRjZWNmOTFjMWNkOGY3ODAzNzZiZjU1NCIsIm5iZiI6MTc0MTkwODEyMS4wNjksInN1YiI6IjY3ZDM2ODk5ZmFjMTYzMGMyNjAyNmVkMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.t2fmtDgkZ89rHqB8C9WF7BzzKCAVoizYCSm4sAxq8zo";
+const URL = "https://api.themoviedb.org/3/search/movie";
+const token = import.meta.env.VITE_TMDB_TOKEN;
 
 export const fetchMovies = async (query: string): Promise<Movie[]> => {
-  const url = `${axios.defaults.baseURL}3/search/movie?query=${query}&include_adult=false&language=en-US`;
-  const params = {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
+  const response = await axios.get<MovieHttpResponse>(URL, {
+    params: {
+      query,
+      include_adult: false,
+      language: "en-US",
+      page: 1,
     },
-  };
-  try {
-    const response = await axios.get<{ results: Movie[] }>(url, params);
-
-    return response.data.results || [];
-  } catch (error) {
-    console.error("Error fetching movies:", error);
-    return [];
-  }
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data.results;
 };
+
+export const imgURL = "https://image.tmdb.org/t/p/w500";
