e58c2a27eba7a4ce3f23280c5e864cafe55b1c4b
Леонiд Куташев <leo.kutashev@gmail.com>
2025-09-21 13:28:42 +0200

    fix 1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import ReactPaginate from "react-paginate";
 import toast, { Toaster } from "react-hot-toast";
 import type { Movie } from "../../types/movie";
 import { fetchMovies } from "../../services/movieService";
-
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import styles from "./App.module.css";
 
 import SearchBar from "../SearchBar/SearchBar";
@@ -13,56 +13,65 @@ import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import MovieModal from "../MovieModal/MovieModal";
 
 export default function App() {
-  // STATE
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [isError, setIsError] = useState<boolean>(false);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [movie, setMovie] = useState<Movie | null>(null);
+  const { data, isPending, isError, isSuccess } = useQuery({
+    queryKey: ["movie", query, page],
+    queryFn: () => fetchMovies(query, page),
+    enabled: !!query,
+    placeholderData: keepPreviousData,
+  });
 
-  // MODAL FUNCTION
-
-  function isActive(): void {
-    setIsOpen((prev) => !prev);
-    if (isOpen == true && movie != null) setMovie(null);
-  }
-
-  function onSelect(params: Movie): void {
-    setMovie(params);
-    isActive();
-  }
-
-  // SEARCH
-  const handleSearch = async (search: string): Promise<void> => {
-    try {
-      setIsLoading(true);
-      setIsError(false);
-      setMovies([]);
-
-      const data = await fetchMovies(search);
-      setMovies(data);
-
-      if (!data || data.length === 0) {
-        toast.error("No movies found.");
-      }
-    } catch (error) {
-      setIsError(true);
-      console.log(error);
-    } finally {
-      setIsLoading(false);
+  useEffect(() => {
+    if (query && data && data.results.length === 0) {
+      toast("No movies found for your request.");
     }
+  }, [data, query]);
+
+  const handleSubmit = (newQuery: string) => {
+    setQuery(newQuery);
+    setPage(1);
   };
 
-  // BODY
   return (
-    <div className={styles.app}>
-      <Toaster position="top-center" reverseOrder={false} />
-      <SearchBar onSubmit={handleSearch} />
-      {isLoading && <Loader />}
+    <>
+      <SearchBar onSubmit={handleSubmit} />
+
+      {}
+      {query && isPending && !data && <Loader />}
+
       {isError && <ErrorMessage />}
-      {movies.length > 0 && <MovieGrid movies={movies} onSelect={onSelect} />}
-      {isOpen && movie && <MovieModal onClose={isActive} movie={movie} />}
-    </div>
+
+      {isSuccess && data.results.length > 0 && (
+        <>
+          {data.total_pages > 1 && (
+            <ReactPaginate
+              pageCount={data.total_pages}
+              pageRangeDisplayed={5}
+              marginPagesDisplayed={1}
+              onPageChange={({ selected }) => setPage(selected + 1)}
+              forcePage={page - 1}
+              containerClassName={styles.pagination}
+              activeClassName={styles.active}
+              nextLabel="→"
+              previousLabel="←"
+            />
+          )}
+          <MovieGrid
+            movies={data.results}
+            onSelect={(movie: Movie) => setSelectedMovie(movie)}
+          />
+        </>
+      )}
+
+      {selectedMovie && (
+        <MovieModal
+          movie={selectedMovie}
+          onClose={() => setSelectedMovie(null)}
+        />
+      )}
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-normalize/modern-normalize.css";
 import "./index.css";
-import App from "./components/App/App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+import App from "./components/App/App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <Toaster /> {}
+    </QueryClientProvider>
+  </React.StrictMode>
 );
